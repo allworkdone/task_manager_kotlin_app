5e44f4805c18538d3624f0fd74e85fc6d9d14f2e
allworkdone <raj635321@gmail.com>
2024-07-01 12:19:42 +0530

    Task Manager: Task completed mentioned in description Center align all of the content vertically and horizontally on the screen. Set the first Text composable to a Bold font weight, 24dp padding top, and 8dp padding bottom. Set the second Text composable to a 16sp font size. refrence: https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-composables-practice-problems#2

--- app/src/main/java/com/allworkdone/taskmanager/MainActivity.kt
+++ app/src/main/java/com/allworkdone/taskmanager/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.allworkdone.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +29,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    TaskManagerApp(
+//                        Stringify the string resource
+                        heading = stringResource(R.string.heading),
+                        content = stringResource(R.string.content),
+                        modifier = Modifier
+                            .padding(innerPadding)
+
                     )
                 }
             }
@@ -31,17 +44,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManagerApp(heading: String,content: String, modifier: Modifier=Modifier) {
+//    Inserted the image
+    val image = painterResource(id = R.drawable.ic_task_completed)
+//    wrapped the items in the column
+    Column(modifier = modifier.padding(16.dp)
+        //        fill the screen
+        .fillMaxSize(),
+//        arranging the items in the column
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Image(painter = image, contentDescription = null)
+        TaskManagerText(
+            heading = heading,
+            content = content,
+            modifier = Modifier
+
+        )
+    }
+}
+
+@Composable
+fun TaskManagerText(heading: String,content: String, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = heading,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = content,
+            modifier = modifier,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        TaskManagerApp(stringResource(R.string.heading), stringResource(R.string.content))
     }
 }
